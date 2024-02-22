72de9c1cc208f961ead6e704bf9dfa26e17a2ea9
shwetapund <pundshweta2020@gmail.com>
2024-02-22 01:25:59 -0800

    feat: Registraiotn is crste

---
+++ server/model/Registration.js
@@ -0,0 +1,24 @@
+import { Schema, model} from "mongoose";
+
+const UserSchema = new Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    mobile:{
+        type:String,
+        required:true
+    }
+})
+
+const Regi = model('Regi',UserSchema);
+
+export default Regi
