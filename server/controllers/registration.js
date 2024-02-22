c89c3877b056ab54a7724a0d16ddb66cae89130b
shwetapund <pundshweta2020@gmail.com>
2024-02-22 03:08:57 -0800

    feat: login api create

--- server/controllers/registration.js
+++ server/controllers/registration.js
@@ -18,4 +18,35 @@ const signupApi = async (req,res)=>{
         message:"Signup Successfully"
     })
 }
-export {signupApi}
+
+const loginApi = async (req,res)=>{
+    const {email,password} = req.body;
+
+    if(!email || !password){
+        res.json({
+            success:false,
+            message:"enter valid email or password !"
+        })
+    }
+    const findUser = await Regi.findOne({
+        email:email,
+        password:password
+    }).select('name email mobile')
+
+    if(!findUser){
+        res.json({
+            success: false,
+            message:"user not found"
+        })
+    }
+
+    const savelogin = await findUser.save();
+
+    res.json({
+        success:true,
+        data:savelogin,
+        message:"login successfully"
+    })
+}
+
+export {signupApi, loginApi}
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import {signupApi} from "./controllers/registration.js";
+import {signupApi, loginApi} from "./controllers/registration.js";
 
 const app = express();
 app.use(express.json());
@@ -12,13 +12,16 @@ const PORT = process.env.PORT || 5000
 const connectMongoDB = async() =>{
     const connection = await mongoose.connect(process.env.MONGODB_URI)
     if(connection){
-        console.log("mongoDB iis connect 💖")
+        console.log("mongoDB is connect 💖")
     }
 }
 connectMongoDB();
 
 //signup
 app.post('/api/v1/signup',signupApi)
+
+//login
+app.post('/api/v1/login',loginApi)
 
 app.listen(PORT, ()=>{
     console.log(`server is running on ${PORT}`)
