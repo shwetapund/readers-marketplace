3d31139f42b9ddea0e81d73b84a6de7f2223d444
shwetapund <pundshweta2020@gmail.com>
2024-02-22 20:19:17 -0800

    feat: API create for update the books

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,25 +1,52 @@
-import {Book} from "./../model/books.js";
+import { Book } from "./../model/books.js";
 
-const bookApi = async (req,res)=>{
-    const {cover, title, description, genre, publishDate, price, tags} = req.body;
+const bookApi = async (req, res) => {
+  const { cover, title, description, genre, publishDate, price, tags } =
+    req.body;
 
-    const newbooks = new Book({
+  const newbooks = new Book({
+    cover,
+    title,
+    description,
+    genre,
+    publishDate,
+    price,
+    tags,
+  });
+
+  const savedBooks = await newbooks.save();
+
+  res.json({
+    success: true,
+    data: savedBooks,
+    message: "successfully added books",
+  });
+};
+
+const updateBooksApi = async (req, res) => {
+  const { _id } = req.params;
+  const { cover, title, description, genre, publishDate, price, tags } =
+    req.body;
+
+    await Book.updateOne({ _id: _id },
+    {
+      $set: {
         cover,
         title,
         description,
         genre,
         publishDate,
         price,
-        tags
-    })
+        tags,
+      },
+    }
+  );
+  const updatebooks = await Book.findOne({ _id: _id });
+  res.json({
+    success: true,
+    data: updatebooks,
+    message: "successfully book update",
+  });
+};
 
-    const savedBooks = await newbooks.save();
-
-    res.json({
-        success:true,
-        data:savedBooks,
-        message:"successfully added books"
-    })
-    
-}
-export {bookApi};
+export { bookApi, updateBooksApi };
