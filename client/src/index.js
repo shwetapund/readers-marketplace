b54bea8d0756a417014af70f908a70b38607613a
shwetapund <pundshweta2020@gmail.com>
2024-02-22 23:05:36 -0800

    feat: create simple react app and create ad folder structure

---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./views/Home/Home";
+import About from "./views/About/About";
+import Signup from "./views/Signup/Signup";
+import Login from "./views/Login/Login";
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<Home />
+  },
+  {
+    path:'/about',
+    element:<About />
+  },
+  {
+    path:'/signup',
+    element:<Signup />
+  },
+  {
+    path:'/login',
+    element:<Login />
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router}/>
+);
+
